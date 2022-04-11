b90dedb8e9a7d137322cb49f1c4a790938a1dc90
james yow jun wei <jamesyjw-wm19@student.tarc.edu.my>
2022-04-12 04:11:24 +0800

    Create FindCustomerAct

---
+++ app/src/main/java/com/example/grobmadass/FindcustomerActivity.kt
@@ -0,0 +1,18 @@
+package com.example.grobmadass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+
+
+
+
+class FindcustomerActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_findcustomer)
+
+    }
+}
