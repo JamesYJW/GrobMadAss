3791fc1220faa18ff13d8e692ae9236bf0138475
james yow jun wei <jamesyjw-wm19@student.tarc.edu.my>
2022-04-15 03:56:25 +0800

    fix bug for data binding edit find customer layout

--- app/src/main/java/com/example/grobmadass/FindcustomerActivity.kt
+++ app/src/main/java/com/example/grobmadass/FindcustomerActivity.kt
@@ -3,13 +3,11 @@ package com.example.grobmadass
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
-
-
+import com.example.grobmadass.databinding.ActivityFindcustomerBinding
 
 
 class FindcustomerActivity : AppCompatActivity() {
-
-
+    private lateinit var binding: ActivityFindcustomerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_findcustomer)
