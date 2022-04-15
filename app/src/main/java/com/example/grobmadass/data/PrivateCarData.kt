4eb7eb84696d4ad36e081388740bcf1cf772db7a
james yow jun wei <jamesyjw-wm19@student.tarc.edu.my>
2022-04-16 00:29:06 +0800

    add PrivateCarActivity add data folder add PrivateCarData add dependency of recycleview

---
+++ app/src/main/java/com/example/grobmadass/data/PrivateCarData.kt
@@ -0,0 +1,15 @@
+package com.example.grobmadass.data
+
+data class PrivateCarData(
+    var privateCarId: String = "",
+    var privateCarWaitGeoN: Double = 0.0,
+    var privateCarWaitGeoE: Double = 0.0,
+    var privateCarDesGeoN: Double = 0.0,
+    var privateCarDesGeoE: Double = 0.0,
+    var privateCarTotalPax: Int = 0,
+    var privateCarTotalTime: Int = 0,
+    var privateCarTotalDistance: Double = 0.0,
+    var privateCarTotalPrice: Double = 0.0,
+    var privateCarStatus: Int = 0,
+    var customerId: String = "",
+)
