b4001b199381306b834d14e086d68d71e4f5cf4f
james yow jun wei <jamesyjw-wm19@student.tarc.edu.my>
2022-04-16 03:51:49 +0800

    Add all driver UI Page Edit PrivateCarActivity

---
+++ app/src/main/java/com/example/grobmadass/CarpoolingCarActivity.kt
@@ -0,0 +1,11 @@
+package com.example.grobmadass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CarpoolingCarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_carpooling_car)
+    }
+}
