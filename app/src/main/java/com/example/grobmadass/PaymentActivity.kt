43fb4be0b45eafc28d194b92627091b515a283fc
mumuDara <puihockming2@gmail.com>
2022-04-17 04:02:01 +0800

    PAYMENT UPDATE 0.1

--- app/src/main/java/com/example/grobmadass/PaymentActivity.kt
+++ app/src/main/java/com/example/grobmadass/PaymentActivity.kt
@@ -3,35 +3,51 @@ package com.example.grobmadass
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import com.example.grobmadass.databinding.ActivityPaymentBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class PaymentActivity : AppCompatActivity() {
 
+
     private lateinit var binding: ActivityPaymentBinding
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPaymentBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        database = FirebaseDatabase.getInstance().getReference("PrivateCar")
 
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.title = "Pricing"
 
         //TODO: Change text view value follow by data car type etc from here
-        val pricingCarType : String = binding.textViewPricingCar.text.toString()
-        val pricingDistance : String = binding.textViewPricingKM.text.toString()
-        val pricingTime : String = binding.textViewPricingTime.text.toString()
-        val pricingCost : String = binding.textViewPricingCost.text.toString()
+
 
         val buttonAddPromo : ImageView = binding.btnApplyPromoCodeIcon
         val buttonMakePayment : Button = binding.btnMakePayment
+        val pcID:String ="PCID00001"
+        database.child(pcID).get().addOnSuccessListener {
+            binding.textViewPricingCar.text = it.child("privateCarId").value.toString()
+            binding.textViewPricingKM.text = it.child("privateCarTotalDistance").value.toString()
+            binding.textViewPricingTime.text = it.child("privateCarTotalTime").value.toString()
+            binding.textViewPricingCost.text= it.child("privateCarTotalPrice").value.toString()
 
-        val passObj : String = pricingCarType+"_"+pricingDistance+"_"+pricingTime+"_"+pricingCost
+
+        }
 
         buttonAddPromo.setOnClickListener {
+
+            val pricingCarType : String = binding.textViewPricingCar.text.toString()
+            val pricingDistance : String = binding.textViewPricingKM.text.toString()
+            val pricingTime : String = binding.textViewPricingTime.text.toString()
+            val pricingCost : String = binding.textViewPricingCost.text.toString()
+            val passObj : String = pcID+"_"+pricingCarType+"_"+pricingDistance+"_"+pricingTime+"_"+pricingCost
             val bIntent = Intent(this, PromoCodeActivity::class.java)
             bIntent.putExtra("receiveObj",passObj) //pass obj
             startActivity(bIntent)
@@ -39,6 +55,11 @@ class PaymentActivity : AppCompatActivity() {
         }
 
         buttonMakePayment.setOnClickListener {
+            val pricingCarType : String = binding.textViewPricingCar.text.toString()
+            val pricingDistance : String = binding.textViewPricingKM.text.toString()
+            val pricingTime : String = binding.textViewPricingTime.text.toString()
+            val pricingCost : String = binding.textViewPricingCost.text.toString()
+            val passObj : String = pcID+"_"+pricingCarType+"_"+pricingDistance+"_"+pricingTime+"_"+pricingCost+"_0"
 
             val bIntent = Intent(this, PaymentSummaryActivity::class.java)
             bIntent.putExtra("receiveObj",passObj) //pass obj
@@ -47,6 +68,7 @@ class PaymentActivity : AppCompatActivity() {
         }
 
     }
+
 
 
 
